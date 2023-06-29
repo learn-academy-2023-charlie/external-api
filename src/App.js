93cd37acf49a62db84f22901a3a93159d905c42b
SunkissedQueen <baxtercm@outlook.com>
2023-06-28 20:42:24 -0700

    initial setup for lecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import mockNasaData from "./mockNasaData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const getIP = () => {
+    console.log("Aloha, the getIP function is working")
+  }
+
+  return(
+    <>
+      <h1>External API</h1>
+      <button onClick={getIP}>Click to see your IP info</button>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/mockNasaData.js
@@ -0,0 +1,203 @@
+const mockNasaData = {
+  photos: [
+    {
+      id: 424907,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631290503689E01_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424908,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631290305226E03_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424909,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631280503688E0B_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424910,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631280305225E03_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424911,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631270503687E03_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424912,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631270305224E03_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424913,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631260503686E03_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424914,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631260305223E03_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424915,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631250503685E01_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424916,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631250305222E03_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424917,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631240503684E03_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424918,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631240305221E03_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424919,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631230503683E01_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    },{
+      id: 424920,
+      sol: 1000,
+      camera: { id: 22, name: "MAST", rover_id: 5, full_name: "Mast Camera" },
+      img_src:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631230305220E02_DXXX.jpg",
+      earth_date: "2015-05-30",
+      rover: {
+        id: 5,
+        name: "Curiosity",
+        landing_date: "2012-08-06",
+        launch_date: "2011-11-26",
+        status: "active"
+      }
+    }
+  ]
+}
+
+export default mockNasaData
